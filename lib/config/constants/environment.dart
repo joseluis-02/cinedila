fa514ff9fa17c9c1a958e392fcf1add36d3c9f91
Joseluis Gutierrez <joseluis.c.jlgl@gmail.com>
2024-05-03 18:18:43 -0400

    Variables de entorno configurado

---
+++ lib/config/constants/environment.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Environment {
+  static String theMovieApiKey =
+      dotenv.env['THE_MOVIEDB_KEY'] ?? 'No hay el Key';
+}
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinedila/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,6 +7,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text(Environment.theMovieApiKey),
+      ),
+    );
   }
 }
